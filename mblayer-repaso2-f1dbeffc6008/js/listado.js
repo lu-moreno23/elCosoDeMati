efeb0de45391ed06f1d2b830a2d2fc8c3965a4c3
Eduardo Sanchez <eduardo@Eduardos-Air.fibertel.com.ar>
2018-09-10 21:35:58 -0300

    Agregamos listados y armamos constructor

--- mblayer-repaso2-f1dbeffc6008/js/listado.js
+++ mblayer-repaso2-f1dbeffc6008/js/listado.js
@@ -3,4 +3,16 @@
 const listNombres = ['Pc Mac', 'Auriculares', 'Pc Mac Otro Modelo', 'Tablet', 'Auriculares Grises', 'Pc MSI color negro', 'Celular', 'Pc Asus', 'Camara de fotos'];
 const precios = [45000, 2000, 37000, 1500, 4000, 16000, 8000, 22000, 999];
 const stock = [1,42, 3, 200, 342, 10, 100, 6, 0]
+const detalles = [
+"Es un producto nuevo", 
+"Es un producto usado",
+"Es un producto nuevo", 
+"Es un producto usado",
+"Es un producto nuevo", 
+"Es un producto usado",
+"Es un producto usado",
+"Es un producto usado",
+"Es un producto usado",
+]
+
 
---
+++ mblayer-repaso2-f1dbeffc6008/js/modelo.js
@@ -0,0 +1,22 @@
+const Producto = function (nombre, numeroImagen, precio, detalles, stock) {
+    this.nombre = nombre,
+    this.imagen = "../img/product" + numeroImagen +".png"
+    this.precio = precio,
+    this.detalle = detalles,
+    this.stock = stock
+    this.favorito = false
+};
+
+function crearProductos () {
+
+    for (let i = 0; i < listNombres.length; i++) {
+        let valorImagen = i + 1;
+        const nuevoProducto = new Producto(listNombres[i], valorImagen, precios[i], detalles[i], stock[i])
+        console.log(nuevoProducto)
+    }
+}
+
+//"../img/product" + [i]++ +".png"
+
+
+// Agregar un Integrer que compruebe que precio es igual a un número.
